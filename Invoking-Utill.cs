49471ec8de537e6b158b3d75fbae53015b02a573
Unknown-Coder <ninjanigeljoe@gmail.com>
2024-10-15 18:57:29 -0400

    Invoking-Utill.cs

--- Invoking-Utill.cs
+++ Invoking-Utill.cs
@@ -1,95 +1,121 @@
-using System.Reflection;
 using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
-public static class ReflectionExtensions
+namespace Util
 {
-    public static ReflectionUtil<T> Sys<T>(this T obj)
+    //Idea from MainCool
+    public class INVOKEUTILL<R>
     {
-        return new ReflectionUtil<T>(obj);
-    }
-}
+        private const BindingFlags PrivateInstance = BindingFlags.NonPublic | BindingFlags.Instance;
+        private const BindingFlags PrivateStatic = BindingFlags.NonPublic | BindingFlags.Static;
+        private const BindingFlags PrivateField = PrivateInstance | BindingFlags.GetField;
+        private const BindingFlags PrivateProperty = PrivateInstance | BindingFlags.GetProperty;
+        private const BindingFlags StaticField = PrivateStatic | BindingFlags.GetField;
+        private const BindingFlags StaticProperty = PrivateStatic | BindingFlags.GetProperty;
+        private const BindingFlags PrivateMethod = PrivateInstance | BindingFlags.InvokeMethod;
+        private const BindingFlags StaticMethod = PrivateStatic | BindingFlags.InvokeMethod;
+        private const BindingFlags InternalMethod = BindingFlags.NonPublic | BindingFlags.InvokeMethod;
 
-public class ReflectionUtil<T>
-{
-    private readonly T targetObject;
+        private R Object { get; }
+        private Type Type { get; }
 
-    public ReflectionUtil(T targetObject)
-    {
-        this.targetObject = targetObject;
-    }
-
-    public void InvokeMethod(string methodName, params object[] args)
-    {
-        Type targetType = typeof(T);
-        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
-
-        MethodInfo method = targetType.GetMethod(methodName, bindingFlags);
-
-        if (method == null)
+        internal INVOKEUTILL(R obj)
         {
-            throw new ArgumentException($"Method '{methodName}' not found in type '{targetType}'.");
+            Object = obj ?? throw new ArgumentNullException(nameof(obj));
+            Type = typeof(R);
         }
 
-        method.Invoke(targetObject, args);
-    }
-
-    public object GetFieldValue(string fieldName)
-    {
-        Type targetType = typeof(T);
-        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField;
-
-        FieldInfo field = targetType.GetField(fieldName, bindingFlags);
-
-        if (field == null)
+        private T GetValue<T>(string name, BindingFlags flags)
         {
-            throw new ArgumentException($"Field '{fieldName}' not found in type '{targetType}'.");
+            try
+            {
+                var field = Type.GetField(name, flags);
+                return field != null ? (T)field.GetValue(Object) : default;
+            }
+            catch { return default; }
         }
 
-        return field.GetValue(targetObject);
-    }
-
-    public void SetFieldValue(string fieldName, object value)
-    {
-        Type targetType = typeof(T);
-        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetField;
-
-        FieldInfo field = targetType.GetField(fieldName, bindingFlags);
-
-        if (field == null)
+        private T GetProperty<T>(string name, BindingFlags flags)
         {
-            throw new ArgumentException($"Field '{fieldName}' not found in type '{targetType}'.");
+            try
+            {
+                var property = Type.GetProperty(name, flags);
+                return property != null ? (T)property.GetValue(Object) : default;
+            }
+            catch { return default; }
         }
 
-        field.SetValue(targetObject, value);
-    }
-
-    public object GetPropertyValue(string propertyName)
-    {
-        Type targetType = typeof(T);
-        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty;
-
-        PropertyInfo property = targetType.GetProperty(propertyName, bindingFlags);
-
-        if (property == null)
+        private INVOKEUTILL<R> SetValue(string name, object value, BindingFlags flags)
         {
-            throw new ArgumentException($"Property '{propertyName}' not found in type '{targetType}'.");
+            try
+            {
+                var field = Type.GetField(name, flags);
+                if (field != null) field.SetValue(Object, value);
+            }
+            catch { }
+            return this;
         }
 
-        return property.GetValue(targetObject);
-    }
-
-    public void SetPropertyValue(string propertyName, object value)
-    {
-        Type targetType = typeof(T);
-        BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty;
-
-        PropertyInfo property = targetType.GetProperty(propertyName, bindingFlags);
-
-        if (property == null)
+        private INVOKEUTILL<R> SetProperty(string name, object value, BindingFlags flags)
         {
-            throw new ArgumentException($"Property '{propertyName}' not found in type '{targetType}'.");
+            try
+            {
+                var property = Type.GetProperty(name, flags);
+                if (property != null) property.SetValue(Object, value);
+            }
+            catch { }
+            return this;
         }
 
-        property.SetValue(targetObject, value);
+        private T InvokeMethod<T>(string name, BindingFlags flags, params object[] args)
+        {
+            try
+            {
+                var method = Type.GetMethods(flags).FirstOrDefault(m =>
+                    m.Name == name && m.GetParameters().Length == args.Length &&
+                    m.GetParameters().Select(p => p.ParameterType).SequenceEqual(args.Select(a => a?.GetType() ?? typeof(object))));
+                return method != null ? (T)method.Invoke(Object, args) : default;
+            }
+            catch { return default; }
+        }
+
+        public T GetValue<T>(string name, bool isStatic = false, bool isProperty = false)
+        {
+            var flags = isProperty ? (isStatic ? StaticProperty : PrivateProperty) : (isStatic ? StaticField : PrivateField);
+            return isProperty ? GetProperty<T>(name, flags) : GetValue<T>(name, flags);
+        }
+
+        public INVOKEUTILL<R> SetValue(string name, object value, bool isStatic = false, bool isProperty = false)
+        {
+            var flags = isProperty ? (isStatic ? StaticProperty : PrivateProperty) : (isStatic ? StaticField : PrivateField);
+            return isProperty ? SetProperty(name, value, flags) : SetValue(name, value, flags);
+        }
+
+        public T InvokeInternalMethod<T>(string name, params object[] args) => InvokeMethod<T>(name, InternalMethod, args);
+        public T Invoke<T>(string name, params object[] args) => InvokeMethod<T>(name, PrivateMethod, args);
+
+        public object GetValue(string name, bool isStatic = false, bool isProperty = false) => GetValue<R>(name, isStatic, isProperty);
+
+        public INVOKEUTILL<R> Invoke(string name, params object[] args) => Invoke<R>(name, args)?.Ref();
+
+        public bool HasField(string name) => Type.GetField(name, PrivateField) != null || Type.GetField(name, StaticField) != null;
+        public bool HasProperty(string name) => Type.GetProperty(name, PrivateProperty) != null || Type.GetProperty(name, StaticProperty) != null;
+        public bool HasMethod(string name) => Type.GetMethods(PrivateMethod).Any(m => m.Name == name) || Type.GetMethods(StaticMethod).Any(m => m.Name == name);
+
+        public Type GetFieldType(string name) => Type.GetField(name, PrivateField)?.FieldType ?? Type.GetField(name, StaticField)?.FieldType;
+
+        public Type GetPropertyType(string name) => Type.GetProperty(name, PrivateProperty)?.PropertyType ?? Type.GetProperty(name, StaticProperty)?.PropertyType;
+
+        public object[] GetMethodParameters(string name) => Type.GetMethods(PrivateMethod)
+            .Where(m => m.Name == name)
+            .Select(m => m.GetParameters().Select(p => p.ParameterType).ToArray())
+            .FirstOrDefault();
+    }
+
+    public static class ReflectorExtensions
+    {
+        public static INVOKEUTILL<R> Ref<R>(this R obj) => new INVOKEUTILL<R>(obj);
     }
 }
